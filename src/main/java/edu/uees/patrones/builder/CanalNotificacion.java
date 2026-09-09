6fd6a1c588fb26fe61e7ac5d50195bdf0e2617e5
Edison Romero <edison.romero@uees.edu.ec>
2026-09-08 20:45:23 -0500

    feat: implementar reserva builder

---
+++ src/main/java/edu/uees/patrones/builder/CanalNotificacion.java
@@ -0,0 +1,9 @@
+package edu.uees.patrones.builder;
+
+/** Canal de notificacion preferido para una {@link Reserva}. */
+public enum CanalNotificacion {
+    EMAIL,
+    SMS,
+    PUSH,
+    WHATSAPP
+}
