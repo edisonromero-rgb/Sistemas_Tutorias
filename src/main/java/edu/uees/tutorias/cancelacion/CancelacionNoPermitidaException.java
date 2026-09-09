42fe33683bd540215342f1f864f67e88c6ca88c9
edisonromero-rgb <edison.romero@uees.edu.ec>
2026-09-08 20:05:54 -0500

    chore: limpiar repositorio para reiniciar entregas

--- src/main/java/edu/uees/tutorias/cancelacion/CancelacionNoPermitidaException.java
+++
@@ -1,9 +0,0 @@
-package edu.uees.tutorias.cancelacion;
-
-/** Se lanza cuando una {@link PoliticaCancelacion} rechaza una cancelacion. */
-public class CancelacionNoPermitidaException extends RuntimeException {
-
-    public CancelacionNoPermitidaException(String mensaje) {
-        super(mensaje);
-    }
-}
--- src/main/java/edu/uees/tutorias/domain/Modalidad.java
+++
@@ -1,7 +0,0 @@
-package edu.uees.tutorias.domain;
-
-/** Modalidad en la que se dicta una tutoria. Campo opcional de {@link Reserva}. */
-public enum Modalidad {
-    PRESENCIAL,
-    VIRTUAL
-}
